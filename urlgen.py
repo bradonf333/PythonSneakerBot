f0dda2aa337ddb8e57d8b79aa2184cdfba432c1d
bradonf333 <bradonf123@gmail.com>
2017-07-18 19:46:26 -0600

    Converted size to float for calculation & added input

--- urlgen.py
+++ urlgen.py
@@ -4,7 +4,7 @@ def URLGen(model, size):
     BaseSize = 580
     #Base Size for the Shoe is 6.5
     #Minus 6.5 from the shoe size to give a base, 6.5 would equal 0
-    ShoeSize = size - 6.5
+    ShoeSize = float(size) - 6.5
     #Number that we need to add to base size to equal the desired shoe size
     ShoeSize = ShoeSize * 20
     #Gives us the desired shoe size code
@@ -14,5 +14,9 @@ def URLGen(model, size):
     #Build the URL
     URL = 'http://www.adidas.com/us/nmd_r1-shoes/' + str(model) + '.html?forceSelSize=' + str(model) + '_' + str(ShoeSizeCode)
     return URL
+Model = input('Model #')
+Size = input('Size: ')
 
-print(URLGen('BZ0220',9.5))
+URL = URLGen(Model, Size)
+
+print(str(URL))
