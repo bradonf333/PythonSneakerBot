2f54e94093531e30b2738a441c8c48dae2341b50
bradonf333 <bradonf123@gmail.com>
2017-07-18 19:08:24 -0600

    Got the raw size code

--- urlgen.py
+++ urlgen.py
@@ -1,1 +1,11 @@
 # Base URL = http://www.adidas.com/us/nmd_r1-shoes/BZ0220.html?forceSelSize=BZ0220_640
+
+def URLGen(model, size):
+        BaseSize = 580
+        #Base Size for the Shoe is 6.5
+        #Minus 6.5 from the shoe size to give a base, 6.5 would equal 0
+        ShoeSize = size - 6.5
+        #Number that we need to add to base size to equal the desired shoe size
+        ShoeSize = ShoeSize * 20
+        #Gives us the desired shoe size code
+        RawSize = ShoeSize + BaseSize
