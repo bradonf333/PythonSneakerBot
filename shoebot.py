696312c48acfb29c3cf7728bca4313adceef9c1d
bradonf333 <bradonf123@gmail.com>
2017-07-19 21:18:29 -0600

    Format comments

--- shoebot.py
+++ shoebot.py
@@ -10,16 +10,16 @@ import requests
 def url_gen(model, size):
     "function_docstring"
     base_size = 580
-    #Base Size for the Shoe is 6.5
-    #Minus 6.5 from the shoe size to give a base, 6.5 would equal 0
+    # Base Size for the Shoe is 6.5
+    # Minus 6.5 from the shoe size to give a base, 6.5 would equal 0
     shoe_size = float(size) - 6.5
-    #Number that we need to add to base size to equal the desired shoe size
+    # Number that we need to add to base size to equal the desired shoe size
     shoe_size = shoe_size * 20
-    #Gives us the desired shoe size code
+    # Gives us the desired shoe size code
     raw_size = shoe_size + base_size
-    #Convert RawSize to int
+    # Convert RawSize to int
     shoe_size_code = int(raw_size)
-    #Build the URL
+    # Build the URL
     url = 'http://www.adidas.com/us/nmd_r1-shoes/' + str(model) + '.html?forceSelSize=' + str(model) + '_' + str(shoe_size_code)
     return url
 def check_stock(url, model):
